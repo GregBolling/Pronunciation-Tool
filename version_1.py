b33a69855f74951d19fa476a702a8e872b9b31a1
bgolden1 <brodygolden7@gmail.com>
2021-04-02 16:16:56 -0500

    Default running system

--- version_1.py
+++ version_1.py
@@ -4,6 +4,15 @@ from nltk.corpus import brown
 import random
 import os
 from pocketsphinx import Decoder, get_model_path
+from flask import Flask, session
+app = Flask(__name__)
+app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
+sent = ""
+mic = speech_recog.Microphone()
+recog = speech_recog.Recognizer()
+evaluation_sents = ['time', 'bad', 'can', 'teacher', 'fat', 'red', 'bat', 'cheap', 'shark', 'shop', 'bake']
 
 
 def generate_random_sent():
@@ -12,39 +21,88 @@ def generate_random_sent():
     return corpus[rand]
 
 
-if __name__ == '__main__':
-    mic = speech_recog.Microphone()
-    recog = speech_recog.Recognizer()
-    model_path = get_model_path()
+@app.route("/") # take note of this decorator syntax, it's a common pattern
+def homepage():
+    session['index'] = 0
+    return '''
+        <h3 align="center">Welcome! Would you like to get started?</h3>
+        <p align="center">
+            <a href=input >
+                <button class=grey style="height:75px;width:150px">
+                    Yes
+                </button>
+            </a>
+        </p>
+    '''
 
-    choice = input("Ready to start?(y/n): ")
-    while choice == 'y' or choice == 'Y':
-        with mic as audio_file:
-            print("Say: Time can be a good teacher")
 
-            recog.adjust_for_ambient_noise(audio_file)
-            audio_data = recog.listen(audio_file)
+@app.route("/input")
+def input():
+    sent = evaluation_sents[session['index']]
+    return '''
+            <h3 align="center">Please read the following sentence: {}</h3>
+            <p align="center">
+                <a href=recognizer >
+                    <button class=grey style="height:75px;width:150px">
+                        Press to record
+                    </button>
+                </a>
+            </p>
+            '''.format(sent)
 
-            print("Converting Speech to Text...")
 
-            raw_data = audio_data.get_raw_data(convert_rate=16000,
-                                               convert_width=2)  # the included language models require audio to be 16-bit mono 16 kHz in little-endian format
+@app.route('/recognizer')
+def recognizer():
+    with mic as audio_file:
+        model_path = get_model_path()
+        recog.adjust_for_ambient_noise(audio_file)
+        audio_data = recog.listen(audio_file)
 
-            # Create a decoder with a certain model
-            config = Decoder.default_config()
-            config.set_string('-hmm', os.path.join(model_path, 'en-us'))
-            config.set_string('-allphone', os.path.join(model_path, 'en-us-phone.lm.dmp'))
-            config.set_float('-lw', 2.0)
-            config.set_float('-beam', 1e-10)
-            config.set_float('-pbeam', 1e-10)
-            decoder = Decoder(config)
+        print("Converting Speech to Text...")
 
-            decoder.start_utt()  # begin utterance processing
-            decoder.process_raw(raw_data, False,
-                                True)  # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
-            decoder.end_utt()  # stop utterance processing
+        raw_data = audio_data.get_raw_data(convert_rate=16000,
+                                           convert_width=2)  # the included language models require audio to be 16-bit mono 16 kHz in little-endian format
 
-            print('Best hypothesis segments:', [seg.word for seg in decoder.seg()])
+        # Create a decoder with a certain model
+        config = Decoder.default_config()
+        config.set_string('-hmm', os.path.join(model_path, 'en-us'))
+        config.set_string('-allphone', os.path.join(model_path, 'en-us-phone.lm.dmp'))
+        config.set_float('-lw', 2.0)
+        config.set_float('-beam', 1e-10)
+        config.set_float('-pbeam', 1e-10)
+        decoder = Decoder(config)
 
-            print("You said: " + recog.recognize_sphinx(audio_data))
-            choice = input("Wanna play again?(y/n)")
+        decoder.start_utt()  # begin utterance processing
+        decoder.process_raw(raw_data, False,
+                            True)  # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
+        decoder.end_utt()  # stop utterance processing
+
+        result = [seg.word for seg in decoder.seg()]
+        if session['index'] < len(evaluation_sents):
+            session['index'] += 1
+            return '''
+                <h3 align="center">You said the following: {}</h3>
+                    <p align="center">
+                        <a href=input >
+                            <button class=grey style="height:75px;width:150px">
+                                Next
+                            </button>
+                        </a>
+                    </p>
+                '''.format(result)
+        else:
+            index = 0
+            return '''
+            <h3 align="center">You said the following: {}</h3>
+                <p align="center">
+                    <a href=input >
+                        <button class=grey style="height:75px;width:150px">
+                            Try again?
+                        </button>
+                    </a>
+                </p>
+            '''.format(result)
+
+
+if __name__ == "__main__":
+    app.run()
